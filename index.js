98ae9c952b9dd876d23d029b75ca3fb1c96b8608
shaktawat19 <harshshaktawat.tech@gmail.com>
2025-05-18 13:11:06 +0530

    database config

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import { connectToMongoDB } from './config/db.config.js'
 
 dotenv.config()
 
 const app = express()
+connectToMongoDB()
+
 const port = 3000
 
 app.listen(port, () => {
