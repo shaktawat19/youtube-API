8d005fc5d74994981409ea41927161b4a7a1a22b
shaktawat19 <harshshaktawat.tech@gmail.com>
2025-05-18 13:01:50 +0530

    Running first Server

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+const port = 3000
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
